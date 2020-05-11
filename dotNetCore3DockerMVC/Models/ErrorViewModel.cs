d713751ec1dafedf4a71291eab14538cbb8c8d4f
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-05-11 11:29:04 +0300

    Add project files.

---
+++ dotNetCore3DockerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNetCore3DockerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
